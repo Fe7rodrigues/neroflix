dec9406c225ffa388934a1911a5881f218d9d095
Fernando Rodrigues <98523060+Fe7rodrigues@users.noreply.github.com>
2022-07-15 09:49:37 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const listaFilmes = [
+    "https://pt.wikipedia.org/wiki/Supernatural_(s%C3%A9rie_de_televis%C3%A3o)",
+    "https://pt.wikipedia.org/wiki/Never_Back_Down",
+    "https://pt.wikipedia.org/wiki/Stranger_Things",
+    "https://pt.wikipedia.org/wiki/Hacksaw_Ridge",
+    "https://pt.wikipedia.org/wiki/Spider-Man:_No_Way_Home",
+    "https://pt.wikipedia.org/wiki/Prison_Break",
+    "https://pt.wikipedia.org/wiki/The_Walking_Dead_(s%C3%A9rie_de_televis%C3%A3o)",
+    "https://pt.wikipedia.org/wiki/The_Longest_Yard",
+    "https://pt.wikipedia.org/wiki/Ford_v_Ferrari",
+    "https://pt.wikipedia.org/wiki/Smallville",
+    "https://pt.wikipedia.org/wiki/Cobra_Kai",
+    "https://pt.wikipedia.org/wiki/Dexter_(s%C3%A9rie_de_televis%C3%A3o)"
+  ];
+  
+  function informacao(filme) {
+    window.open(listaFilmes[filme]);
+  }
